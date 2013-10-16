af20a615b7a0ebef667de15d260fc3d170e3fd81
Jerome Etienne <jerome.etienne@gmail.com>
2013-10-16 18:54:56 +0200

    typo fix

--- package.require.js
+++ package.require.js
@@ -2,5 +2,5 @@ define( [ 'module'	// to set .baseURL
 	, './threex.spaceships'
 	], function(module){
 	// set baseUrl for this extension
-	THREEx.SpaceShips.baseURL	= module.uri+'/../';
+	THREEx.SpaceShips.baseUrl	= module.uri+'/../';
 });
